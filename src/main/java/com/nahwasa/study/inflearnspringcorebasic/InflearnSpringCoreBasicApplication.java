b63aa281cb834120ce59c0650a9d9ed21552f684
nahwasa <nahwasa@gmail.com>
2023-02-27 21:20:13 +0900

    🎉 begin a project

---
+++ src/main/java/com/nahwasa/study/inflearnspringcorebasic/InflearnSpringCoreBasicApplication.java
@@ -0,0 +1,13 @@
+package com.nahwasa.study.inflearnspringcorebasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnSpringCoreBasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InflearnSpringCoreBasicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nahwasa/study/inflearnspringcorebasic/InflearnSpringCoreBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nahwasa.study.inflearnspringcorebasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnSpringCoreBasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
