fedcf2c0380a35d925994a79554962641ff05505
nahwasa <nahwasa@gmail.com>
2023-03-11 22:26:14 +0900

    [새로운 할인 정책 개발]

--- src/main/java/com/nahwasa/study/inflearnspringcorebasic/discount/RateDiscountPolicy.java
+++ src/main/java/com/nahwasa/study/inflearnspringcorebasic/discount/RateDiscountPolicy.java
@@ -3,14 +3,17 @@ package com.nahwasa.study.inflearnspringcorebasic.discount;
 import com.nahwasa.study.inflearnspringcorebasic.member.Grade;
 import com.nahwasa.study.inflearnspringcorebasic.member.Member;
 
-public class FixDiscountPolicy implements DiscountPolicy {
+/**
+ * 추가 요구사항 - 정률% 할인 추가
+ */
+public class RateDiscountPolicy implements DiscountPolicy {
 
-    private int discountFixAmount = 1000;
+    private int discountPercent = 10;
 
     @Override
     public int discount(final Member member, final int price) {
         if (member.getGrade() == Grade.VIP) {
-            return discountFixAmount;
+            return price * discountPercent / 100;
         }
         return 0;
     }
