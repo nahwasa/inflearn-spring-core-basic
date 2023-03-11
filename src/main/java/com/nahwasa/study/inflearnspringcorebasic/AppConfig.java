510fe8b17b12f22d29a293d4fa9b08f708fed3e7
nahwasa <nahwasa@gmail.com>
2023-03-11 23:09:20 +0900

    AppConfig 리팩토링

--- src/main/java/com/nahwasa/study/inflearnspringcorebasic/AppConfig.java
+++ src/main/java/com/nahwasa/study/inflearnspringcorebasic/AppConfig.java
@@ -13,10 +13,18 @@ import com.nahwasa.study.inflearnspringcorebasic.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    private static MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    private static FixDiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
